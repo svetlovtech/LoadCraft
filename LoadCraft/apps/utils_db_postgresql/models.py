068bb5301897fb5ddbf58bbfde42453e4d53ff4e
32-52 <alexeysvetlov92@gmail.com>
2019-09-30 00:37:12 +0300

    Added models for postgresql extractor

--- LoadCraft/apps/utils_db_postgresql/models.py
+++ LoadCraft/apps/utils_db_postgresql/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PostgresqlSettings(models.Model):
+    label_id = models.CharField(max_length=30, primary_key=True)
+    dbname = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
+    host = models.CharField(max_length=150)
+    port = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.label_id} dbname:{self.dbname} username:{self.username}'
