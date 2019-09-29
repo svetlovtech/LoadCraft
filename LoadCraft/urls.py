41fd5850141001e451fee18e75cacfb46ed4b023
Alexey Svetlov <a.svetlov@pflb.ru>
2019-09-29 17:12:43 +0300

    Added grappelli urls

--- LoadCraft/urls.py
+++ LoadCraft/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    path('utils_db_postgresql/', include('utils_db_postgresql.urls')),
 ]
