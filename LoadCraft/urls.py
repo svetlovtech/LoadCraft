a0b6df75d9b8bb7f9011b0cebed523463d31633a
32-52 <alexeysvetlov92@gmail.com>
2019-10-05 19:31:59 +0300

    Added drf-yasg

--- LoadCraft/urls.py
+++ LoadCraft/urls.py
@@ -15,11 +15,36 @@ Including another URLconf
 """
 from django.contrib.auth.models import User
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import serializers, viewsets, routers
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+                                               cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+                                             cache_timeout=0), name='schema-redoc'),
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
+    
     path('api/v1/utils_db_postgresql/', include('utils_db_postgresql.urls')),
 ]
