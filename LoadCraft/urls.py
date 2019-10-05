62f130650fcc9d412083b441f8c0ff6d860771c3
32-52 <alexeysvetlov92@gmail.com>
2019-10-06 02:08:29 +0300

    Edited api information

--- LoadCraft/urls.py
+++ LoadCraft/urls.py
@@ -25,12 +25,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
-        default_version='v1',
+        title="LoadCraft API",
+        default_version='v1910',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        contact=openapi.Contact(email="alexeysvetlov92@gmail.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
